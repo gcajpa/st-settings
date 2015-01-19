982f1836ca942f87f6e724070992973c3f850384
Gustavo Corrêa Alves <gcajpa@gmail.com>
2015-01-19 01:39:36 -0200

    Customizacoes

---
+++ Packages/User/env.py
@@ -0,0 +1,12 @@
+import os
+
+home = os.environ["HOME"]
+
+os.environ["PATH"] = "%s/local/bin:%s/local/node/npm/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" % (home, home)
+# os.environ["PATH"] = "%s/local/bin:%s/local/ruby/gems/2.1/bin:%s/local/node/npm/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin" % (home, home, home)
+#os.environ["GEM_HOME"] = "%s/local/ruby/gems/2.1" % (home)
+# os.environ["GEM_PATH"] = os.environ["GEM_HOME"]
+
+# print ("PATH=%s" % os.environ["PATH"])
+# print ("GEM_HOME=%s" % os.environ["GEM_HOME"])
+# print ("GEM_PATH=%s" % os.environ["GEM_PATH"])
